b19d53f6b80f2e4b74fe23b1125e0922ebb9f1f9
Sanjay Makvana <Timglp@1618>
2024-11-18 21:41:57 +0000

    blind-auction: Rename application name

--- Auction-Service/src/main/java/db/blindauction/auctionservice/AuctionService.java
+++
@@ -1,7 +0,0 @@
-package db.blindauction.auctionservice;
-
-public class AuctionService {
-    public static void main(String[] args) {
-        System.out.println("AuctionService ");
-    }
-}
---
+++ Auction-Service/src/main/java/db/blindauction/auctionservice/AuctionServiceApplication.java
@@ -0,0 +1,7 @@
+package db.blindauction.auctionservice;
+
+public class AuctionServiceApplication {
+    public static void main(String[] args) {
+        System.out.println("AuctionServiceApplication ");
+    }
+}
