2d8fe5ab46b4453054092cd06f8d74539343a172
Sanjay Makvana <Timglp@1618>
2024-11-18 15:12:21 +0000

    blind-auction: Initial maven project setup

---
+++ Auction-Service/src/main/java/db/blindauction/auctionservice/AuctionService.java
@@ -0,0 +1,7 @@
+package db.blindauction.auctionservice;
+
+public class AuctionService {
+    public static void main(String[] args) {
+        System.out.println("AuctionService ");
+    }
+}
---
+++ user-service/src/main/java/db/blindauction/userservice/UserService.java
@@ -0,0 +1,7 @@
+package db.blindauction.userservice;
+
+public class UserService {
+    public static void main(String[] args) {
+        System.out.println("UserService");
+    }
+}
