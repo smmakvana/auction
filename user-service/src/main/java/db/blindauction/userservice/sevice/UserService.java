8ae0d8d7e3e5e0ccc1576be2cafd3b6fcb0df05c
Jay <jayvekariya2003@gmail.com>
2025-01-13 19:11:57 +0530

    Add 5 bidder users and 3 seller users with respective tokens to the repository

--- user-service/src/main/java/db/blindauction/userservice/sevice/UserService.java
+++ user-service/src/main/java/db/blindauction/userservice/sevice/UserService.java
@@ -34,9 +34,49 @@ public class UserService {
             user3.setName("Gamma");
             user3.setToken("token3");
 
+            User user4 = new User();
+            user4.setName("Delta");
+            user4.setToken("token4");
+
+            User user5 = new User();
+            user5.setName("Hexa");
+            user5.setToken("token5");
+
+            User user6 = new User();
+            user6.setName("Theta");
+            user6.setToken("token6");
+
+            User user7 = new User();
+            user7.setName("Eta");
+            user7.setToken("token7");
+
+            User user8 = new User();
+            user8.setName("Zeta");
+            user8.setToken("token8");
+
+            User user9 = new User();
+            user9.setName("Iota");
+            user9.setToken("token9");
+
+            User user10 = new User();
+            user10.setName("Kappa");
+            user10.setToken("token10");
+
+            User user11 = new User();
+            user11.setName("Lambda");
+            user11.setToken("token11");
+
             userRepository.save(user1);
             userRepository.save(user2);
             userRepository.save(user3);
+            userRepository.save(user4);
+            userRepository.save(user5);
+            userRepository.save(user6);
+            userRepository.save(user7);
+            userRepository.save(user8);
+            userRepository.save(user9);
+            userRepository.save(user10);
+            userRepository.save(user11);
         }
     }
 }
